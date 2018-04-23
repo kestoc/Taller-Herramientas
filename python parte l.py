ed38ce9712a5451ab57fbcdd0429ce3f3c030b67
Daniel Fernando Sosa Suárez <danielsosa117@javerianacali.edu.co>
2018-04-23 17:56:05 -0500

    commit kestok
    parte de kevin ocampo

---
+++ python parte l.py
@@ -0,0 +1,33 @@
+clave = [["a", "n"], ["b", "o"], ["c", "p"], ["d", "q"], ["e", "r"], ["f", "s"], ["g", "t"],
+         ["h", "u"], ["i", "v"], ["j", "w"], ["k", "x"], ["l", "y"], ["m", "z"], ["n", "a"],
+         ["o", "b"], ["p", "c"], ["q", "d"], ["r", "e"], ["s", "f"], ["t", "g"], ["u", "h"],
+         ["v", "i"], ["w", "j"], ["x", "k"], ["y", "l"], ["z", "m"], ["A", "N"], ["B", "O"],
+         ["C", "P"], ["D", "Q"], ["E", "R"], ["F", "S"], ["G", "T"], ["H", "U"], ["I", "V"],
+         ["J", "W"], ["K", "X"], ["L", "Y"], ["M", "Z"], ["N", "A"], ["O", "B"], ["P", "C"],
+         ["Q", "D"], ["R", "E"], ["S", "F"], ["T", "G"], ["T", "H"], ["V", "I"], ["W", "J"],
+         ["X", "K"], ["Y", "L"], ["Z", "M"]]
+
+lista = ["n", "y", " ", "n", "g", "n", "d", "h", "r"]
+
+
+def traducir(l, lc):
+    """
+    l = lista a traducir
+    lc = la grande
+    """
+    temp = []
+    c = 0
+    while (c < len(l)):
+        a = l[c]
+        i = 0
+        while (i < len(lc)):
+            if (a == lc[i][0]):
+                temp.append(lc[i][1])
+            else:
+                if (a == " "):
+                    temp.append(" ")
+                    break
+            i += 1
+        c += 1
+
+    return temp
